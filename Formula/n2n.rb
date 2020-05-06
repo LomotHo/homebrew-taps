f6c579412ae88c42f44053af0f53210caf3a5a17
lomot <lomot@qq.com>
2020-05-06 11:50:00 +0800

    add n2n

---
+++ Formula/n2n.rb
@@ -0,0 +1,18 @@
+class N2n < Formula
+  desc "n2n v2.6 for macOS"
+  homepage ""
+  url "https://github.com/ntop/n2n/archive/2.6.tar.gz"
+  sha256 "d08a75cba32394e7077d8b3afdef913d61cdf5911141effb5cd3ee2d79c66bc2"
+
+  def install
+    system "./autogen.sh"
+    system "./configure"
+    system "make"
+    bin.install "edge"
+    bin.install "supernode"
+  end
+
+  test do
+    system "false"
+  end
+end
