7282d9c4315eadfc270c444c42131eed7c374c91
lomot <lomot@qq.com>
2021-11-02 13:28:35 +0800

    n2n-3.0

---
+++ Formula/n2n@2.8.rb
@@ -0,0 +1,18 @@
+class N2n < Formula
+  desc "n2n v2.8 for macOS"
+  homepage ""
+  url "https://github.com/ntop/n2n/archive/2.8.tar.gz"
+  sha256 "ad9ab95a79dcf6b412c36489f50de54bb54417b5cea854eb54722b6cb8f0fd04"
+  depends_on "cmake"
+
+  def install
+    system "cmake ."
+    system "make"
+    bin.install "edge"
+    bin.install "supernode"
+  end
+
+  test do
+    system "false"
+  end
+end
---
+++ Formula/n2n@3.0.rb
@@ -0,0 +1,18 @@
+class N2n < Formula
+  desc "n2n v3.0 for macOS"
+  homepage ""
+  url "https://github.com/ntop/n2n/archive/3.0.tar.gz"
+  sha256 "25fcabba7bfcf25f4c9cd7fecc7ce11de48beb0b0f3506053d8485604ea8f50d"
+  depends_on "cmake"
+
+  def install
+    system "cmake . -DCMAKE_BUILD_TYPE=Release"
+    system "make"
+    bin.install "edge"
+    bin.install "supernode"
+  end
+
+  test do
+    system "false"
+  end
+end
