c66afdb518606b0bce8cb9a99da321a7fdb001d8
lomot <lomot@qq.com>
2021-11-06 18:57:57 +0800

    fix

--- Formula/n2n@2.8.rb
+++ Formula/n2n@2.8.rb
@@ -3,10 +3,11 @@ class N2nAT28 < Formula
   homepage ""
   url "https://github.com/ntop/n2n/archive/2.8.tar.gz"
   sha256 "ad9ab95a79dcf6b412c36489f50de54bb54417b5cea854eb54722b6cb8f0fd04"
-  depends_on "cmake"
+  depends_on "automake"
 
   def install
-    system "cmake ."
+    system "./autogen.sh"
+    system "./configure"
     system "make"
     bin.install "edge"
     bin.install "supernode"
