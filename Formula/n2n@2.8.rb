a5b0ec29280cc3e312fcd6bbde1bde403cd67b8b
lomot <lomot@qq.com>
2021-11-06 19:01:50 +0800

    fix

--- Formula/n2n@2.8.rb
+++ Formula/n2n@2.8.rb
@@ -4,6 +4,7 @@ class N2nAT28 < Formula
   url "https://github.com/ntop/n2n/archive/2.8.tar.gz"
   sha256 "ad9ab95a79dcf6b412c36489f50de54bb54417b5cea854eb54722b6cb8f0fd04"
   depends_on "automake"
+  depends_on "openssl"
 
   def install
     system "./autogen.sh"
